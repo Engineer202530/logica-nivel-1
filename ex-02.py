777b584d52320e95a0a5b890bae8efcd6ed1f0ab
Engineer202530 <joaolucasmmatos8@gmail.com>
2025-03-10 21:58:35 -0300

    Create ex-02.py

---
+++ ex-02.py
@@ -0,0 +1,7 @@
+# 1. Frase na tela - Implemente um programa que imprima na tela a frase "O primeiro programa a gente nunca esquece!".
+
+print("Nome: João Lucas")
+
+print("Estado; Rio de Janeiro")
+
+print("Município: Duque de Caxias")
