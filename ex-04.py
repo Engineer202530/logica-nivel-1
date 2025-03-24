ac7a2ec3e45e60c9f183f03cc8888a7368e7e309
Engineer202530 <joaolucasmmatos8@gmail.com>
2025-03-24 20:35:08 -0300

    Update ex-04.py

--- ex-04.py
+++ ex-04.py
@@ -1,6 +1,6 @@
 #. Quadrado - Escrever um programa que imprima a seguinte figura:
-XXXXX
-X   X
-X   X
-X   X
-XXXXX
+print ("xxxxx")
+print ("x   x")
+print ("x   x")
+print ("x   x")
+print ("xxxxx")
